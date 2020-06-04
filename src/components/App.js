12ca77366522aae24a7a0c63c96b7dc3a27cbf8f
o0pmitev <pmitev89@gmail.com>
2020-06-04 09:48:24 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/App.scss';
+import ChatBox from './ChatBox';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ChatBox extends React.Component {
+  render () {
+    return (
+      <div className="chat-box">
+        
+      </div>
+    )
+  }
+}
+
+export default ChatBox;
